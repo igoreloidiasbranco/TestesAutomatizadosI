e98b0264bd2d709e5029b7bfcc8b7d28fde5a476
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2025-04-07 21:35:36 -0300

    Commit inicial

---
+++ projetowebii/src/main/java/tech/ada/projetowebii/ProjetowebiiApplication.java
@@ -0,0 +1,13 @@
+package tech.ada.projetowebii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowebiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowebiiApplication.class, args);
+	}
+
+}
