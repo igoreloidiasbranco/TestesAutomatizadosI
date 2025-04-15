0827cebce59836a4c028b7766f33e15167e88ab6
owie <marketing@letscode.com.br>
2025-04-15 00:05:25 -0300

    feat: Adicionado Put, testado pelo Postman e H2

---
+++ projetowebii/src/main/java/tech/ada/projetowebii/service/AtualizarSerieService.java
@@ -0,0 +1,36 @@
+package tech.ada.projetowebii.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.ada.projetowebii.dto.AtualizarSerieDTO;
+import tech.ada.projetowebii.model.Serie;
+import tech.ada.projetowebii.repository.SerieRepository;
+
+@Service
+public class AtualizarSerieService {
+
+    private final SerieRepository repository;
+
+    @Autowired
+    public AtualizarSerieService(SerieRepository repository) {
+        this.repository = repository;
+    }
+
+    public Serie atualizar(Long id, AtualizarSerieDTO dto) {
+        return repository.findById(id).map(serie -> {
+            if (dto.getTitulo() != null) {
+                serie.setTitulo(dto.getTitulo());
+            }
+            if (dto.getGenero() != null) {
+                serie.setGenero(dto.getGenero());
+            }
+            if (dto.getTemporadas() != null) {
+                serie.setTemporadas(dto.getTemporadas());
+            }
+            if (dto.getAnoLancamento() != null) {
+                serie.setAnoLancamento(dto.getAnoLancamento());
+            }
+            return repository.save(serie);
+        }).orElseThrow(() -> new RuntimeException("Série com ID " + id + " não encontrada."));
+    }
+}
